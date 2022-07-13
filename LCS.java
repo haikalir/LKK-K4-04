227cd930936956f5533a367a93bd9c614d8ec56c
Yehezkiel Stephanus Austin <105846814+YehezkielSaragih@users.noreply.github.com>
2022-07-13 09:56:51 +0700

    LCS Algoritma - Yehezkiel

--- LCS.java
+++ LCS.java
@@ -44,6 +44,48 @@ class Table{
  
     Table(String[] arrX, String[] arrY){
 
+        this.arrX = arrX;
+        this.arrY = arrY;
+
+        int m = arrX.length;
+        int n = arrY.length;
+
+        // Make array
+        b = new String[m + 1][n + 1];
+        c = new int[m + 1][n + 1];
+
+        // Fill array
+        for (int i = 1; i <= m; i++) {
+            
+            c[i][0] = 0;
+        }
+        for (int j = 0; j <= n; j++) {
+            
+            c[0][j] = 0;
+        }
+
+        // Algorithm
+        for (int i = 1; i <= m; i++) {
+            
+            for (int j = 1; j <= n; j++) {
+                
+                if(arrX[i - 1].equals(arrY[j - 1])){
+
+                    c[i][j] = c[i - 1][j - 1] + 1;
+                    b[i][j] = "\\";
+                }
+                else if(c[i - 1][j] >= c[i][j - 1]){
+
+                    c[i][j] = c[i - 1][j];
+                    b[i][j] = "|";
+                }
+                else{
+
+                    c[i][j] = c[i][j - 1];
+                    b[i][j] = "-";
+                }
+            }
+        }
     }
  
     void printTable(){
