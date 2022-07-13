d2a938638f757f4a50b0dcbefa74b803f70b95cc
Yehezkiel Stephanus Austin <105846814+YehezkielSaragih@users.noreply.github.com>
2022-07-13 09:54:57 +0700

    MCM Print - Yehezkiel

--- MCM.java
+++ MCM.java
@@ -1,19 +1,28 @@
 public class MCM {
+	
 	static char matrix;
+	
 	static void matrixChainOrder(int arr[]) {
+		
 		int length = arr.length;
 		int[][] total = new int[length][length];
 		int[][] kArr = new int[length][length];
 		for (int i = 1; i < length; i++) {
+			
 			total[i][i] = 0;
 		}
 		for (int l = 2; l < length; l++) {
+			
 			for (int i = 1; i < length - l + 1; i++) {
+				
 				int j = i + l - 1;
 				total[i][j] = Integer.MAX_VALUE;
+				
 				for (int k = i; k <= j - 1; k++) {
+					
 					int q = total[i][k] + total[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
 					if (q < total[i][j]) {
+						
 						total[i][j] = q;
 						kArr[i][j] = k;
 					}
@@ -23,14 +32,18 @@ public class MCM {
 		matrix = 'A';
 		System.out.println("\nTabel Total");
 		for (int i = 0; i < total.length; i++) {
+			
 			for (int j = 0; j < total.length; j++) {
+			
 				System.out.print(total[i][j] + "\t");
 			}
 			System.out.println("");
 		}
 		System.out.println("\nTabel K");
 		for (int i = 0; i < kArr.length; i++) {
+			
 			for (int j = 0; j < kArr.length; j++) {
+			
 				System.out.print(kArr[i][j] + "\t");
 			}
 			System.out.println("");
@@ -44,6 +57,14 @@ public class MCM {
 	
 	static void printParenthesis(int i, int j, int n, int[][] arr) {
 		
+		if (i == j) {
+		    System.out.print(matrix++);
+		    return;
+		}
+		System.out.print("(");
+		printParenthesis(i, arr[i][j], n, arr);
+		printParenthesis(arr[i][j] + 1, j, n, arr);
+		System.out.print(")");
 	}
 	
 	public static void main(String[] args) {
