757281e94b0dfd0600c1c08fecdfede2e0132fa4
Raditya Permadhi <101965052+ZenGeming@users.noreply.github.com>
2022-07-13 10:03:58 +0700

    MCM Main - Raditya

--- MCM.java
+++ MCM.java
@@ -68,6 +68,15 @@ public class MCM {
 	}
 	
 	public static void main(String[] args) {
-		
-	}
+		System.out.println("MCM\n");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Masukkan jumlah P");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Masukkan P");
+		for (int i = 0; i < n; i++) {
+		    arr[i] = sc.nextInt();
+		}
+        	matrixChainOrder(arr);
+    	}
 }
